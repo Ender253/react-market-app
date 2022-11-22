e682bdc80e26b472c00a9fbc4cdcc93dc200c813
Ender253 <szollosicasian@gmail.com>
2022-11-22 16:42:55 +0200

    V2.0

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -2,10 +2,11 @@ import classes from "./Input.module.css";
 import React from "react";
 
 const Input = React.forwardRef((props, ref) => {
+  const {label, input,} = props;
   return (
     <div className={classes.input}>
-      <label htmlFor={props.input.id}>{props.label}</label>
-      <input ref ={ref} {...props.input} />
+      <label htmlFor={input.id}>{label}</label>
+      <input ref ={ref} {...input} />
     </div>
   );
 });
